2482edbc0f4f18a8de530ecd96ccaa567abfe4d6
sishirsubedi <subedisishir@gmail.com>
2018-11-08 09:59:43 -0600

    pmkb added

---
+++ HMVV/src/hmvv/gui/mutationlist/tables/PmkbTable.java
@@ -0,0 +1,36 @@
+package hmvv.gui.mutationlist.tables;
+
+import hmvv.gui.HMVVTableColumn;
+import hmvv.gui.mutationlist.MutationListFrame;
+import hmvv.gui.mutationlist.tablemodels.PmkbTableModel;
+import hmvv.io.InternetCommands;
+import hmvv.model.Mutation;
+
+public class PmkbTable extends CommonTable {
+    private static final long serialVersionUID = 1L;
+
+    public PmkbTable(MutationListFrame parent, PmkbTableModel model) {
+        super(parent, model);
+    }
+
+    @Override
+    protected HMVVTableColumn[] constructCustomColumns() {
+        return HMVVTableColumn.getCustomColumnArray(model.getColumnCount(), 7);
+    }
+
+    @Override
+    protected void handleMouseClick(int column) throws Exception {
+        if (column == 7) {
+            searchPmkb();
+        }
+    }
+
+    private void searchPmkb() throws Exception {
+        Mutation mutation = getSelectedMutation();
+        String onco_id = mutation.getPmkbID();
+        if (!onco_id.equals("") && !onco_id.equals("null")) {
+            InternetCommands.searchPmkb(onco_id);
+        }
+    }
+}
+
